60b2905fae3bf03b62e808800e5ab04cddc2f060
Junho Won <jwon@c2r8s1.42seoul.kr>
2020-08-06 19:34:45 +0900

    ADD: get_next_line

---
+++ 02_get_next_line/get_next_line_utils_bonus.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jwon <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/09 16:34:18 by jwon              #+#    #+#             */
+/*   Updated: 2020/04/17 16:10:07 by jwon             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *str)
+{
+	int		idx;
+
+	idx = 0;
+	while (str[idx])
+		idx++;
+	return (idx);
+}
+
+char	*ft_strdup(const char *str)
+{
+	int		idx;
+	char	*dest;
+
+	if (!(dest = (char *)malloc(sizeof(char) * (ft_strlen(str) + 1))))
+		return (NULL);
+	idx = 0;
+	while (str[idx])
+	{
+		dest[idx] = str[idx];
+		idx++;
+	}
+	dest[idx] = '\0';
+	return (dest);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	while (*str)
+	{
+		if (*str == c)
+			return ((char *)str);
+		str++;
+	}
+	if (c == '\0')
+		return ((char *)str);
+	return (0);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	unsigned int	idx;
+	size_t			s_len;
+	char			*modified_s;
+
+	if (!s || !(modified_s = malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	s_len = ft_strlen((char *)s);
+	idx = 0;
+	while (start < s_len && s[start + idx] && idx < len)
+	{
+		modified_s[idx] = s[start + idx];
+		idx++;
+	}
+	modified_s[idx] = '\0';
+	return (modified_s);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int			idx;
+	int			idx_join;
+	char		*join;
+
+	if (!s1 || !s2 || !(join = malloc(sizeof(char) *
+					(ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1))))
+		return (NULL);
+	idx = 0;
+	idx_join = 0;
+	while (s1[idx])
+		join[idx_join++] = s1[idx++];
+	idx = 0;
+	while (s2[idx])
+		join[idx_join++] = s2[idx++];
+	join[idx_join] = '\0';
+	return (join);
+}
