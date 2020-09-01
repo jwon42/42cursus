262a174d4e449edb01dd3932b9ab97d54b338b3f
jwon42 <jwon@student.42seoul.kr>
2020-09-01 20:12:00 +0900

    UPDATE: libasm ft_strdup.s

--- 08_libasm/ft_strdup.s
+++ 08_libasm/ft_strdup.s
@@ -1,34 +1,25 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_strdup.s                                        :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: jwon <marvin@42.fr>                        +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/08/16 23:30:18 by jwon              #+#    #+#              #
-#    Updated: 2020/08/16 23:31:13 by jwon             ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+;Prototype
+;char *ft_strdup(const char *s1);
 
 section .text
-global _ft_strdup
+	global _ft_strdup
 	extern _malloc
 	extern _ft_strlen
 	extern _ft_strcpy
 
 _ft_strdup:
-	push rdi
-	call _ft_strlen
-	mov rdi, rax
-	call _malloc
-	cmp rax, 0
-	je exit
-	pop rsi
-	mov rdi, rax
-	sub rsp, 8
-	call _ft_strcpy
-	add rsp, 8
-	jmp exit
+	push	rdi ; 스택에 rdi 저장
+	call	_ft_strlen ; ft_strlen 호출
+	mov		rdi, rax ; rdi에 ft_strlen 반환 값 저장
+	call	_malloc ; malloc 호출
+	cmp		rax, 0 ; malloc으로 할당된 공간과 0(NULL) 비교
+	je		exit ; 같다면 exit로 이동
+	pop		rsi ; 스택에 저장했던 값을 뽑아와서 rsi에 저장
+	mov		rdi, rax ; rdi에 malloc 반환 값 복사
+	sub		rsp, 8 ; 8바이트 만큼 빼서 조정
+	call	_ft_strcpy ; ft_strcpy 호출
+	add		rsp, 8 ; 8바이트 만큼 더해서 조정
+	jmp		exit ; exit로 이동
 
 exit:
-	ret
+	ret ; rax 호출
