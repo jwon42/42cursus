6722f8c064ce12d13f1cbda89b54e28a12d97f14
jwon42 <jwon@student.42seoul.kr>
2020-09-01 20:06:30 +0900

    UPDATE: libasm ft_strcpy.s

--- 08_libasm/ft_strcpy.s
+++ 08_libasm/ft_strcpy.s
@@ -1,32 +1,23 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_strcpy.s                                        :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: jwon <marvin@42.fr>                        +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/08/15 12:44:23 by jwon              #+#    #+#              #
-#    Updated: 2020/08/15 12:47:26 by jwon             ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+;Prototype
+;char *ft_strcpy(const char *dst, const char *src);
 
 section .text
-global _ft_strcpy
+	global _ft_strcpy
 
 _ft_strcpy:
-	mov	rcx, 0
-	mov rbx, 0
-	cmp rsi, 0
-	je	exit
+	mov	rcx, 0 ; rcx 값 0으로 초기화
+	mov	rbx, 0 ; rbx 값 0으로 초기화
+	cmp	rsi, 0 ; rsi와 0(NULL) 비교
+	je	exit ; 같으면 exit으로 이동
 
 copy:
-	mov bl, byte [rsi + rcx]
-	mov byte [rdi + rcx], bl
-	inc rcx
-	cmp bl, 0
-	je	exit
-	jmp copy
+	mov	bl, byte [rsi + rcx] ; bl을 문자열 *src의 rcx번째 문자로 초기화
+	mov	byte [rdi + rcx], bl ; 문자열 *dst의 rcx번째 문자를 bl로 초기화
+	inc	rcx ; rcx++
+	cmp	bl, 0 ; bl과 0(NULL) 비교
+	je	exit ; 같으면 exit으로 이동
+	jmp	copy ; copy로 이동
 
 exit:
-	mov rax, rdi
-	ret
+	mov	rax, rdi ; rax에 rdi 값 복사
+	ret ; rax 값 리턴
