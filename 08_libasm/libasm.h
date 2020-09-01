00862405909135f3f9ee0f8b7bc3ab68a87463f5
jwon42 <jwon@student.42seoul.kr>
2020-09-01 19:49:11 +0900

    UPDATE: libasm libasm.h

--- 08_libasm/libasm.h
+++ 08_libasm/libasm.h
@@ -6,7 +6,7 @@
 /*   By: jwon <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/21 16:11:53 by jwon              #+#    #+#             */
-/*   Updated: 2020/08/21 16:12:02 by jwon             ###   ########.fr       */
+/*   Updated: 2020/09/01 17:14:37 by jwon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,15 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-size_t	ft_strlen(const char *s);
-int		ft_strcmp(const char *s1, const char *s2);
-char	*ft_strcpy(const char *dst, const char *src);
-char	*ft_strdup(const char *s1);
-int		ft_read(int fd, void *buff, size_t nbyte);
-int		ft_write(int fd, void *buff, size_t nbyte);
+# define	RED "\x1b[31m"
+# define	YELLOW "\x1b[33m"
+# define	RESET "\x1b[0m"
+
+size_t		ft_strlen(const char *s);
+int			ft_strcmp(const char *s1, const char *s2);
+char		*ft_strcpy(const char *dst, const char *src);
+char		*ft_strdup(const char *s1);
+ssize_t		ft_read(int fd, void *buff, size_t nbyte);
+ssize_t		ft_write(int fd, void *buff, size_t nbyte);
 
 #endif
