f122a0e0b52d16e4f0099e6ab030dab3731fceff
jwon42 <jwon@student.42seoul.kr>
2020-09-01 19:46:02 +0900

    UPDATE: libasm ft_strlen.s

--- 08_libasm/ft_strlen.s
+++ 08_libasm/ft_strlen.s
@@ -1,27 +1,18 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    ft_strlen.s                                        :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: jwon <marvin@42.fr>                        +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/08/15 12:30:38 by jwon              #+#    #+#              #
-#    Updated: 2020/08/15 12:32:12 by jwon             ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
+;Prototype
+;size_t ft_strlen(const char *s);
 
 section .text
-global _ft_strlen
+	global _ft_strlen
 
 _ft_strlen:
-	mov rax, 0
-	jmp count
+	mov rax, 0 ; 리턴할 raw 값 0으로 초기화
+	jmp count ; count로 이동
 
 count:
-	cmp BYTE [rdi + rax], 0 
-	je	done
-	inc rax
-	jmp count
+	cmp BYTE [rdi + rax], 0 ; 1바이트씩 읽고, 0(NULL) 과 비교
+	je	done ; 만약 NULL 이라면? done 으로 이동
+	inc rax ; rax++
+	jmp count ; count로 이동
 
 done:
-	ret
+	ret ; rax 값 리턴
