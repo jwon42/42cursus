8de22214f69c8eec65a7b591bfb62efdaf1271ac
jwon42 <jwon@student.42seoul.kr>
2020-11-07 19:00:08 +0900

    UPDATE: exam_rank_03

--- 11_exam_rank_03/micro_paint/micro_paint.c
+++ 11_exam_rank_03/micro_paint/micro_paint.c
@@ -6,7 +6,7 @@
 /*   By: jwon <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 14:31:29 by jwon              #+#    #+#             */
-/*   Updated: 2020/10/30 15:52:56 by jwon             ###   ########.fr       */
+/*   Updated: 2020/11/07 18:58:29 by jwon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,15 @@
 
 int ft_strlen(char *str)
 {
-	int	idx;
-	idx = 0;
+	int	idx = 0;
 	while (str[idx])
 		idx++;
 	return (idx);
 }
 
-int	ft_perror(char *msg)
+int	ft_perror(char *str)
 {
-	write(1, msg, ft_strlen(msg));
+	write(1, str, ft_strlen(str));
 	return (1);
 }
 
@@ -46,62 +45,51 @@ int	main(int argc, char *argv[])
 {
 	FILE	*file;
 	char	*paper;
-	int		read;
-	int		pos;
+	int		read, pos;
+	int		x, y;
 
-	int		x;
-	int		y;
-
-	int		b_width;
-	int		b_height;
+	int		b_width, b_height;
 	char	background;
 
-	char	id;
-	float	id_x;
-	float	id_y;
-	float	width;
-	float	height;
-	char	color;
+	char	id, color;
+	float	id_x, id_y, width, height;
 
-	if (argc != 2)
-		return (ft_perror(ERR1));
-	if (!(file = fopen(argv[1], "r")) ||
-		(fscanf(file, "%d %d %c\n", &b_width, &b_height, &background) != 3) ||
-		(!(b_width > 0 && b_width <= 300 && b_height > 0 && b_height <= 300)))
-		return (ft_perror(ERR2));
-	paper = (char *)malloc(sizeof(char) * (b_width * b_height));
-	memset(paper, background, b_width * b_height);
-	while ((read = fscanf(file, "%c %f %f %f %f %c\n", &id, &id_x, &id_y, &width, &height, &color)) == 6)
+	if (argc != 2) // 인자가 없으면
+		return (ft_perror(ERR1)); // 1번 에러 출력, 함수종료
+	if (!(file = fopen(argv[1], "r")) || // 첫번째 인자의 파일을 오픈할 수 없거나
+		(fscanf(file, "%d %d %c\n", &b_width, &b_height, &background) != 3) || // 파일의 첫번째 줄이 조건에 부합하지 않거나
+		(!(b_width > 0 && b_width <= 300 && b_height > 0 && b_height <= 300)) || // 배경이 정해진 범위가 아니거나
+		(!(paper = (char *)malloc(sizeof(char) * (b_width * b_height))))) // 배경만큼을 메모리 할당할 수 없다면
+		return (ft_perror(ERR2)); // 2번 에러 출력, 함수종료
+	memset(paper, background, b_width * b_height); // 배경 그리기
+	while ((read = fscanf(file, "%c %f %f %f %f %c\n", &id, &id_x, &id_y, &width, &height, &color)) == 6) // 파일의 두번째 줄부터 조건(형식, 갯수)에 부합하면 반복
 	{
-		if (!(width > 0 && height > 0) || !(id == 'R' || id == 'r'))
-			break ;
-		y = 0;
-		while (y < b_height)
+		if (!(width > 0 && height > 0) || !(id == 'R' || id == 'r')) // 그려야 하는 크기가 음수거나, id가 'R'또는 'r'이 아니면
+			break ; // 반복문 탈출
+		y = -1;
+		while (++y < b_height) // 배경 높이까지 반복
 		{
-			x = 0;
-			while (x < b_width)
+			x = -1;
+			while (++x < b_width) // 배경 너비까지 반복
 			{
-				pos = check_pos((float)x, (float)y, id_x, id_y, width, height);
+				pos = check_pos((float)x, (float)y, id_x, id_y, width, height); // 경계 체크
 				if (pos == 1 || (pos == 2 && id == 'R'))
-					paper[y * b_width + x] = color;
-				x++;
+					paper[y * b_width + x] = color; // 그리기
 			}
-			y++;
 		}
 	}
-	if (read != -1)
+	if (read != -1) // 반복문 탈출했는데 그 이유가 읽기 실패한게 아니라면? 조건에 부합하지 않는 것이기에
 	{
-		free(paper);
-		return (ft_perror(ERR2));
+		free(paper); // 배경 메모리 해제
+		return (ft_perror(ERR2)); // 2번 에러 출력, 함수종료
 	}
-	y = 0;
-	while (y < b_height)
+	y = -1;
+	while (++y < b_height) // 배경 높이까지 반복
 	{
-		write(1, paper + y * b_width, b_width);
-		write(1, "\n", 1);
-		y++;
+		write(1, paper + y * b_width, b_width); // 그림 그리기
+		write(1, "\n", 1); // 개행
 	}
-	free(paper);
-	fclose(file);
-	return (0);
+	free(paper); // 배경 메모리 해제
+	fclose(file); // 파일 닫기
+	return (0); // 함수 종료
 }
--- 11_exam_rank_03/mini_paint/mini_paint.c
+++ 11_exam_rank_03/mini_paint/mini_paint.c
@@ -6,7 +6,7 @@
 /*   By: jwon <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/30 14:31:29 by jwon              #+#    #+#             */
-/*   Updated: 2020/10/30 17:27:10 by jwon             ###   ########.fr       */
+/*   Updated: 2020/11/07 18:58:28 by jwon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,7 @@
 
 int ft_strlen(char *str)
 {
-	int	idx;
-	idx = 0;
+	int	idx = 0;
 	while (str[idx])
 		idx++;
 	return (idx);
@@ -65,45 +64,42 @@ int	main(int argc, char *argv[])
 	float	radius;
 	char	color;
 
-	if (argc != 2)
-		return (ft_perror(ERR1));
-	if (!(file = fopen(argv[1], "r")) ||
-		(fscanf(file, "%d %d %c\n", &b_width, &b_height, &background) != 3) ||
-		(!(b_width > 0 && b_width <= 300 && b_height > 0 && b_height <= 300)))
-		return (ft_perror(ERR2));
-	paper = (char *)malloc(sizeof(char) * (b_width * b_height));
-	memset(paper, background, b_width * b_height);
-	while ((read = fscanf(file, "%c %f %f %f %c\n", &id, &id_x, &id_y, &radius, &color)) == 5)
+	if (argc != 2) // 인자가 없으면
+		return (ft_perror(ERR1)); // 1번 에러 출력, 함수종료
+	if (!(file = fopen(argv[1], "r")) || // 첫번째 인자의 파일을 오픈할 수 없거나
+		(fscanf(file, "%d %d %c\n", &b_width, &b_height, &background) != 3) || // 파일의 첫번째 줄이 조건에 부합하지 않거나
+		(!(b_width > 0 && b_width <= 300 && b_height > 0 && b_height <= 300)) || // 배경이 정해진 범위가 아니거나
+		(!(paper = (char *)malloc(sizeof(char) * (b_width * b_height))))) // 배경만큼을 메모리 할당할 수 없다면
+		return (ft_perror(ERR2)); // 2번 에러 출력, 함수종료
+	memset(paper, background, b_width * b_height); // 배경 그리기
+	while ((read = fscanf(file, "%c %f %f %f %c\n", &id, &id_x, &id_y, &radius, &color)) == 5) // 파일의 두번째 줄부터 조건(형식, 갯수)에 부합하면 반복
 	{
-		if (!(radius > 0) || !(id == 'C' || id == 'c'))
-			break ;
-		y = 0;
-		while (y < b_height)
+		if (!(radius > 0) || !(id == 'C' || id == 'c')) // 그려야 하는 크기가 음수거나, id가 'C'또는 'c'가 아니면
+			break ; // 반복문 탈출
+		y = -1;
+		while (++y < b_height) // 배경 높이까지 반복
 		{
-			x = 0;
-			while (x < b_width)
+			x = -1;
+			while (++x < b_width) // 배경 너비까지 반복
 			{
-				pos = check_pos((float)x, (float)y, id_x, id_y, radius);
+				pos = check_pos((float)x, (float)y, id_x, id_y, radius); // 경계 체크
 				if (pos == 0 || (pos == 1 && id == 'C'))
-					paper[y * b_width + x] = color;
-				x++;
+					paper[y * b_width + x] = color; // 그리기
 			}
-			y++;
 		}
 	}
-	if (read != -1)
+	if (read != -1) // 반복문 탈출했는데 그 이유가 읽기 실패한게 아니라면? 조건에 부합하지 않는 것이기에
 	{
-		free(paper);
-		return (ft_perror(ERR2));
+		free(paper); // 배경 메모리 해제
+		return (ft_perror(ERR2)); // 2번 에러 출력, 함수종료
 	}
-	y = 0;
-	while (y < b_height)
+	y = -1;
+	while (++y < b_height) // 배경 높이까지 반복
 	{
-		write(1, paper + y * b_width, b_width);
-		write(1, "\n", 1);
-		y++;
+		write(1, paper + y * b_width, b_width); // 그림 그리기
+		write(1, "\n", 1); // 개행
 	}
-	free(paper);
-	fclose(file);
-	return (0);
+	free(paper); // 배경 메모리 해제
+	fclose(file); // 파일 닫기
+	return (0); // 함수 종료
 }
